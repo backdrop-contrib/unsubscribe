0dcae6f18a8cbe7b2fb22154513ee0d61b83e0d2
Matthew Grasmick <madmatter23@gmail.com>
2012-08-23 02:03:20 -0400

    improving api

--- unsubscribe.api.php
+++ unsubscribe.api.php
@@ -1,18 +1,29 @@
 <?php
 
 /**
- * Allows modules to alter unsubscribes blocking of emails to unsubscribed users.
+ * @file
+ * This file contains API documentation for the unsubscribe module. Note that
+ * all of this code is merely for example purposes, it is never executed when
+ * using the unsubscribe module.
  */
-function hook_unsubscribe_mail_block(&$message, &$exemptions, &$block) {
+
+/**
+ * Allows modules to override unsubscribe's blocking function AFTER the block as occured.
+ */
+function hook_unsubscribe_override(&$message) {
+  // More complex exemptions. E.g., exempt any mail sent by user 0.
+  if (user_load_by_mail($message['from'])->uid == 0) {
+    $message['send'] = TRUE;
+  }
+}
+
+/**
+ *
+ */
+function hook_unsubscribe_exemptions_alter(&$exemptions) {
   // Add more exceptions.
   $exemptions[] = 'my_module';
 
   // Remove exemptions.
   unset($exemptions['system']);
-
-  // More complex exemptions. E.g., exempt any mail sent by user 0.
-  if (user_load_by_mail($message['from'])->uid == 0) {
-    $block = FALSE;
-  }
-
 }
